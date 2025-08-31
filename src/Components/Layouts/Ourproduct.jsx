12f9469fc8a474f8acfc0ddb959f46d413187837
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-08-31 22:21:47 +0600

    .

--- src/Components/Layouts/Ourproduct.jsx
+++ src/Components/Layouts/Ourproduct.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import Headding from '../Headding'
-import Container from '../Container'
+import React, { useEffect, useState } from "react";
+import Headding from "../Headding";
+import Container from "../Container";
+import axios from "axios";
 
 const Ourproduct = () => {
+    let [alldata, setalldata]= useState([])
+    useEffect(()=>{
+async function alldatas () {
+   let data=await axios.get("https://mostafakamalasif.github.io/Orebi-API/index.json"  )
+   setalldata(data)
+}
+    },[])
   return (
     <>
-    <div className="py-[94px]">
+      <div className="py-[94px]">
         <Container>
-        <div className="">
-            <Headding as={"h3"} text={<>OUR TRENDY <span className='font-bold'>PRODUCTS</span></>} className={"text-[35px] text-center"}/>
+          <div className="">
+            <Headding
+              as={"h3"}
+              text={
+                <>
+                  OUR TRENDY <span className="font-bold">PRODUCTS</span>
+                </>
+              }
+              className={"text-[35px] text-center"}
+            />
             <div className="pt-[31px]  ">
-            <ul className='flex items-center justify-center gap-x-14 font-medium text-[16px]  '>
-                <li>All</li>
-                <li>NEWARRIVALS</li>
-                <li>  BEST SELLER </li>
-                <li>TOP RATING</li>
-            </ul>
+              <ul className="flex items-center justify-center gap-x-14 font-medium text-gray-600 text-[16px] cursor-pointer">
+                <li className="hover:text-black ">All</li>
+                <li className="hover:text-black ">NEWARRIVALS</li>
+                <li className="hover:text-black "> BEST SELLER </li>
+                <li className="hover:text-black ">TOP RATING</li>
+              </ul>
             </div>
-        </div>
-        </Container>
-    </div>
-    </>
-  )
-}
 
-export default Ourproduct
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Ourproduct;
