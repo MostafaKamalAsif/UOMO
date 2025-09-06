ce27357d832a398b889076658c266b52c5716c7f
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-09-06 12:23:59 +0600

    menu

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -3,12 +3,13 @@ import { FaRegHeart } from "react-icons/fa";
 import { HiOutlineUser } from "react-icons/hi";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { RiMenu2Fill, RiSearchLine } from "react-icons/ri";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import Container from "../Container";
 import Flex from "../Flex";
 import Image from "../Image";
 import Logo from "/src/assets/logo.svg";
 const Header = () => {
+  const location=useLocation();
   return (
     <>
       <div className="pt-8.5 pb-6">
@@ -17,17 +18,21 @@ const Header = () => {
             <div className="">
               <Flex className={"gap-x-[56px]"}>
                 <Image imgsrc={Logo} />
+                {/* useLocation() gives you the current URL (location.pathname) If the current path matches the to value of a Link, we add a bottom border + black textOtherwise, it just stays gray */}
                 <ul className="flex items-center gap-x-11">
                   <Link
                     to="/"
-                   
+                   className={` ${location.pathname === "/"? " border-b-2 border-black ": ""}`}
                     
                   >
-                    <li  className="text-black font-medium ">
+                    <li  className="text-black font-medium ">        
                       HOME
                     </li>
                   </Link>
-                  <Link to="shop" >
+                  <Link to="shop"
+                   className={` ${location.pathname === "/shop"? " border-b-2 border-black ": ""}`}
+                    
+                  >
                     <li  className="text-black font-medium ">
                       SHOP
                     </li>
